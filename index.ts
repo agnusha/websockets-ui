ccdd0238b514363f1b00058cb82e22389783bed6
Agnia Staravoitava <ahniya_staravoitava@epam.com>
2023-07-08 09:23:24 +0200

    inti: change to ts

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { httpServer } from "./src/http_server/index.js";
+import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+
+const wss = new WebSocketServer({ httpServer });
