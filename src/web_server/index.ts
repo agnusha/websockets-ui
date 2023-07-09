543e7e0494b4ce0710bab8580c696731841a6bcc
Agnia Staravoitava <ahniya_staravoitava@epam.com>
2023-07-09 19:03:04 +0200

    feat: add handlers to webSocketServer

--- src/web_server/index.ts
+++ src/web_server/index.ts
@@ -4,13 +4,21 @@ import { WebSocketServer } from 'ws';
 export const createWebSocketServer = (port: number) => {
     const webSocketServer = new WebSocketServer({ port });
 
+
     webSocketServer.on('connection', function connection(ws) {
-    ws.on('error', console.error);
+        ws.on('open', () => {
+            console.log(`Start web socket server on the ${port} port!`);
+        });
 
-    ws.on('message', function message(data) {
-        console.log('received: %s', data);
-    });
+        ws.on('message', function message(data) {
+            const dataParsed = JSON.parse(data.toString());
+            handleMessage(dataParsed);
+        });
 
-    ws.send('something');
+        ws.on('error', console.error);
+        
+        ws.on('close', () => {
+            console.log(`Close web socket server on the ${port} port!`);
+        });
     });
 }
