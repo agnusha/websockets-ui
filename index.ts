e93c4e5dd5cda8fe05b448e31ab911fc82182fbc
Agnia Staravoitava <ahniya_staravoitava@epam.com>
2023-07-09 18:25:05 +0200

    feat: add types and start web socket

--- index.ts
+++ index.ts
@@ -2,8 +2,19 @@ import { httpServer } from "./src/http_server/index";
 import { WebSocketServer } from 'ws';
 
 const HTTP_PORT = 8181;
+const WEB_SOCKET_PORT = 3000;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
-// const wss = new WebSocketServer({ httpServer });
+const wss = new WebSocketServer({ port: WEB_SOCKET_PORT });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
